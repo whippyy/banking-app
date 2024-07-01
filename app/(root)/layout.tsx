93882f5f726fd203efdd510f2674eafbcce76378
whippyy <henry.le2003@gmail.com>
2024-06-30 20:17:22 -0500

    moblie nav bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,28 @@
 import SideBar from "@/components/SideBar";
+import Image from 'next/image'
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const logIn = {firstName: 'Henry', lastName: 'Le' };
+  const loggedIn = {firstName: 'Henry', lastName: 'Le' };
 
   return (
-    <main className="flex h-screen w-full font-inter">
-        <SideBar user = {logIn}/>
-        {children}
+    <main className = "flex h-screen w-full font-inter">
+        <SideBar user = {loggedIn}/>
+
+        <div className = "flex size-full flex-col">
+          <div className = "root-layout">
+            <Image src = "/icons/logo.svg" width={30} height={30} alt = "logo" />
+            <div>
+              <MobileNav user = {loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
+        
     </main>
   );
 }
